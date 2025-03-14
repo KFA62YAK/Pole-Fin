7dcd011aefa0a05fc2cfbb840789cc09770a1e6f
KFA62YAK <chevaliflo@gmail.com>
2025-03-14 12:05:23 +0100

    Update PMtest.py

--- PMtest.py
+++ PMtest.py
@@ -146,7 +146,7 @@ def plot_masculine_graph(selected_graph, player_name, constants, data, positions
             y=regression_line,
             mode="lines",
             name="Progression générale",
-            line=dict(color="green")
+            line=dict(color="darkgreen")
         ))
         position_row = positions[positions["Joueur"] == player_name]
         if not position_row.empty:
